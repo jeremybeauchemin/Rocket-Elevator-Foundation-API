d0620ecdf187067e9c3aef55b7af14dd1c1de313
YOUR NAME <YOUR@EMAIL.com>
2018-11-27 13:16:45 -0500

    bon

--- Controllers/InterventionsController.cs
+++ Controllers/InterventionsController.cs
@@ -45,7 +45,7 @@ namespace RocketApi.Controllers{
                 interventions.Updated_At = DateTime.Now;
                 _context.Interventions.Update(interventions);
                 _context.SaveChanges();
-                return "Interventions number # " + interventions.Id + "Has now changed status from " + previous_status + "to" + status +".";
+                return "Interventions #" + interventions.Id + "Has now changed status from " + previous_status + "to" + status +".";
             }
             {
               if (status == "complete")
@@ -56,7 +56,7 @@ namespace RocketApi.Controllers{
                 interventions.Updated_At = DateTime.Now;
                 _context.Interventions.Update(interventions);
                 _context.SaveChanges();
-                return "Interventions number # " + interventions.Id + " Has now changed status from " + previous_status + " to " + status + ".";  
+                return "Interventions #" + interventions.Id + " Has now changed status from " + previous_status + " to " + status + ".";  
             }
             else
             {
